23a8a16628da427ae31fe28daf230023a51b6d5b
Rabbanihosni10 <rabbanihosni10@gmail.com>
2024-02-16 23:11:21 +0600

    6th

---
+++ Day1_Intro_DOM/js/append.js
@@ -0,0 +1,7 @@
+//1.where to add
+const placeList=document.getElimentById('places-list');
+//2. what to be added
+const li=document.createElement('li');
+li.innerText='Pahartoli Bon';
+//3.add the child   
+placeList.appendChild(li);
