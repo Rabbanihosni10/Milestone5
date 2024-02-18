1ccea81cb6cfc32abcae06579082f3ec61d40b42
Rabbanihosni10 <rabbanihosni10@gmail.com>
2024-02-19 00:08:03 +0600

    2nd.1

--- Day2_Js_DOM_Events/event.js
+++ Day2_Js_DOM_Events/event.js
@@ -1,1 +1,16 @@
+//option1:  directly set on the html element  ( not Always recomended)
 console.log("This is seperate js file!");
+//option 2 : add onclick function  (Always recomended)
+function makeYellow(){
+    document.body.style.backgroundColor='yellow';
+}
+
+//option3(not Always recomended)
+
+const makeOrangeBtn=document.getElementById('make-orange');
+console.log(makeOrangeBtn);
+makeOrangeBtn.onclick=makeBlue1;
+
+function makeBlue1(){
+    document.body.style.backgroundColor='orange';
+}
