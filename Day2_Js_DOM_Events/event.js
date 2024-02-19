82d335d0c58680f2280c44a039678c779f0571bd
Rabbanihosni10 <rabbanihosni10@gmail.com>
2024-02-19 19:56:55 +0600

    3rd.1

--- Day2_Js_DOM_Events/event.js
+++ Day2_Js_DOM_Events/event.js
@@ -1,6 +1,8 @@
 //option1:  directly set on the html element  ( not Always recomended)
+//<button onclick="document.body.style.backgroundColor='yellow'">Make Yellow</button>
 console.log("This is seperate js file!");
 //option 2 : add onclick function  (Always recomended)
+//<button onclick="makeRed()">Make Red</button>
 function makeYellow(){
     document.body.style.backgroundColor='yellow';
 }
@@ -14,3 +16,12 @@ makeOrangeBtn.onclick=makeBlue1;
 function makeBlue1(){
     document.body.style.backgroundColor='orange';
 }
+// const purpleButton=document.getElementById('make-purple');
+//         purpleButton.onclick=function makePurple(){
+//             document.body.style.backgroundColor='purple';    
+//         }
+//option4(another)
+const makeGreen=document.getElementById('make-green');
+makeGreen.addEventListener('click',function makeGreen1(){
+    document.body.style.backgroundColor='green';
+})
